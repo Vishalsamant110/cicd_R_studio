dd2261decce1ab1177fc8a7f4638881ff58bf135
Vishalsamant110 <vishalsamant11@gmail.com>
2022-11-04 21:44:31 +0530

    linter fix

--- R/hello.R
+++ R/hello.R
@@ -22,10 +22,7 @@ hello <- function() {
 #' linter_ex("lint")
 linter_ex <- function(x) {
   if (x == "lint") {
-    if (x == "lint") {
-      if (x == "lint") {
         message("Whoa!")
       }
     }
-  }
-}
+
